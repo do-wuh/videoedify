03ac722a3a32b78da96277b7d25b278504eb6233
dowuh <eskil@tomozy.com>
2019-07-13 20:54:23 +0000

    Flixter 6:30 - lessons order added

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,7 @@ class Lesson < ApplicationRecord
 
   mount_uploader :video, VideoUploader
   mount_uploader :image, ImageUploader
+
+  include RankedModel
+  ranks :row_order, with_same: :section_id
 end
